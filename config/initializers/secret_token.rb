8423bfea090564aa2aa47fe74b2ed7eb69e01645
Brett Hammond <bretthammond@stubthumb.com>
2012-06-05 19:31:12 -0600

    Initial Commit of Rails Application, added Faye gem, added Thin server gem

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FayeTutorial::Application.config.secret_token = 'd556c14a475850e04ea2b5d523924243326351ccd590e031c4aa8c05c08b1a9bf270659c5d086660d8c23c8c59bf696b2c2967add5a41f2a2f28992f7616d215'
